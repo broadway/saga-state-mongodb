e52580c6aaab07ba07a60e711cf7c35ffe1ae00b
othillo <othillo@othillo.nl>
2020-03-04 15:51:10 +0100

    added license

--- src/MongoDBRepository.php
+++ src/MongoDBRepository.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the broadway/broadway-saga package.
+ * This file is part of the broadway/saga-state-mongodb package.
  *
- * (c) Qandidate.com <opensource@qandidate.com>
+ * (c) 2020 Broadway project
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
